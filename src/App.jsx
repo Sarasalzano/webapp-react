f1f8a83a5254a2d75a1ae8a0eab816a224af2371
Sara Salzano <sarasalzano95@gmail.com>
2025-11-03 15:49:41 +0100

    modifica route

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import MainLayout from "./layouts/mainLayout.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/homepage.jsx";
+import MovieDetailPage from "./pages/MovieDetailPage.jsx";
 import './App.css'
 
 function App() {
 
 
-  return (
+return (
   <>
     <BrowserRouter>
-    <Routes>
-      <Route element = {<MainLayout />}>
-    </Route>
-    </Routes>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route index element={<Homepage />} />
+          <Route path="/movie/:id" element={<MovieDetailPage />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </>
-  )
+);
 }
 
 export default App
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,4 +1,5 @@
 import Header from "../components/header";
+import { Outlet } from "react-router-dom";
 
 export default function MainLayout(){
     return(
