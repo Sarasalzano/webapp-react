1c4fd3804b5fd802b01577efa35f6cb5470ba44d
Sara Salzano <sarasalzano95@gmail.com>
2025-11-03 15:48:44 +0100

    esegui card

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function MovieCard(){
+    return(
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                <Link className="btn btn-primary" to="/movie/1">Guarda Dettagli</Link>
+            </div>
+        </div>
+    );
+}
