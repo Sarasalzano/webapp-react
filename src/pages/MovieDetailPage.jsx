012ec6214d5c1f054c559c0b9e665008ddd1ad0b
Sara Salzano <sarasalzano95@gmail.com>
2025-11-03 16:11:38 +0100

    esegui struttura header MovieDetailPage

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,6 +1,24 @@
 
 export default function MovieDetailPage(){
     return(
-    <div>dettagli</div>
+    <>
+    <header>
+        <div className="card mb-3">
+            <div className="row g-0">
+                <div className="col-md-4"></div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h1 className="card-title">Titolo Libro</h1>
+                        <h3 className="sort-by"><i>by Regista</i></h3>
+                        <p className="card-text">Descrizione Film.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section id="reviews">
+        <h4>Our Community Reviews</h4>
+    </section>
+    </>
     );
 }
