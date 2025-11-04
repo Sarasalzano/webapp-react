48da527a1bbdf800f2aaef8fd188e3d061aef9c4
Sara Salzano <sarasalzano95@gmail.com>
2025-11-04 16:21:05 +0100

    aggiungi props e cambia dinamicamente

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
 
-export default function MovieCard(){
-
-    
-    return(
-        <div className="card">
-            <div className="card-body">
-                <img src="" alt="" />
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                <Link className="btn btn-primary" to="/movie/1">Guarda Dettagli</Link>
-            </div>
-        </div>
-    );
+export default function MovieCard({ movie }) {
+  return (
+    <div className="card m-2">
+      <img src={movie.image} alt={movie.title} className="card-img-top"/>
+      <div className="card-body">
+        <h5 className="card-title">{movie.title}</h5>
+        <p className="card-text">{movie.abstract}</p>
+        <Link className="btn btn-primary" to={`/movie/${movie.id}`}>
+          Guarda Dettagli
+        </Link>
+      </div>
+    </div>
+  );
 }
