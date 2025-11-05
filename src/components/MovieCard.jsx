1e14f5f795698c162d80117f5c382c09993575ff
Sara Salzano <sarasalzano95@gmail.com>
2025-11-05 16:26:11 +0100

    imposta dimensioni immagine

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,15 +2,18 @@ import { Link } from "react-router-dom";
 
 export default function MovieCard({ movie }) {
   return (
-    <div className="card m-2">
-      <img src={`http://localhost:3000/imgs/${movie.image}`} alt={movie.title} />
+    <div className="d-flex flex-wrap m-2 align-items-center">
+    <div className="card" style={ { width: "18rem", height: "40rem" }}>
+      <img src={`http://localhost:3000/imgs/${movie.image}`} alt={movie.title} style={ { height: "406px"}}/>
       <div className="card-body">
         <h5 className="card-title">{movie.title}</h5>
         <p className="card-text">{movie.abstract}</p>
         <Link className="btn btn-primary" to={`/movie/${movie.id}`}>
           Guarda Dettagli
         </Link>
-      </div>
     </div>
+</div>
+</div>
   );
 }
+
