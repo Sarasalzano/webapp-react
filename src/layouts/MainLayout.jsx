72dae50a5765fea1d0dbef0eafac99feb318705b
Sara Salzano <sarasalzano95@gmail.com>
2025-11-06 14:55:35 +0100

    inserisci leader in mainlayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,17 @@
 import Header from "../components/header";
 import { Outlet } from "react-router-dom";
+import Loader from "./components/Loader.jsx";
+import { useGlobal } from "../contexts/GlobalContext.jsx";
 
 export default function MainLayout(){
+    // etrapoliamo da context var di stato
+    const { loading } = useGlobal();
     return(
         <>     
         <Header />
         <Outlet />
+        {/* se la variabile di stato è true renderizza componente */}
+        {loading && <Loader />}
         </>
     );
 }
